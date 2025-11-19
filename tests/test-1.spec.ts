996a4a26a7a378905ee2be23d170824af28a24a1
Agumenyuk76 <aaaaagumenyuk@gmail.com>
2025-11-19 16:24:37 +0300

    1

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,54 @@
+import { test, expect } from '@playwright/test';
+
+test('Проверка отображения элементов навигации', async ({ page }) => {
+  await page.goto('https://playwright.help/');
+  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Документация' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'API' })).toBeVisible();
+  await expect(page.getByText('Node.jsNode.jsPythonJava.NET')).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Сообщество' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Обучение', exact: true })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'GitHub repository' })).toBeVisible();
+  await expect(
+    page.getByRole('button', {
+      name: 'Переключение между темным и светлым режимом (сейчас используется Тёмный режим)',
+    }),
+  ).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Поиск (Command+K)' })).toBeVisible();
+});
+test('Проверка названия элементов навигации', async ({ page }) => {
+  await page.goto('https://playwright.help/');
+
+  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toContainText(
+    'Playwright',
+  );
+  await expect(page.getByRole('link', { name: 'Документация' })).toContainText('Документация');
+  await expect(page.getByRole('link', { name: 'API' })).toContainText('API');
+  await expect(page.getByText('Node.jsNode.jsPythonJava.NET')).toContainText('Node.js');
+  await expect(page.getByRole('link', { name: 'Сообщество' })).toContainText('Сообщество');
+  await expect(page.getByRole('link', { name: 'Обучение', exact: true })).toContainText('Обучение');
+});
+test('Проверка атрибутов href элементов навигации ', async ({ page }) => {
+  await page.goto('https://playwright.help/');
+
+  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toHaveAttribute(
+    'href',
+    '/',
+  );
+  await expect(page.getByRole('link', { name: 'Документация' })).toHaveAttribute(
+    'href',
+    '/docs/intro',
+  );
+  await expect(page.getByRole('link', { name: 'API' })).toHaveAttribute(
+    'href',
+    'docs/api/class-playwright',
+  );
+  await expect(page.getByRole('link', { name: 'Сообщество' })).toHaveAttribute(
+    'href',
+    '/community/welcome',
+  );
+  await expect(page.getByRole('link', { name: 'GitHub repository' })).toHaveAttribute(
+    'href',
+    'https://github.com/microsoft/playwright',
+  );
+});
