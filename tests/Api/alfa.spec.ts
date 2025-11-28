b43343fc2a71892886e347bd8cd5b86db2ef2cea
Agumenyuk76 <aaaaagumenyuk@gmail.com>
2025-11-28 14:34:20 +0300

    подтягивание переменной+ откл созд альфа

--- tests/Api/alfa.spec.ts
+++ tests/Api/alfa.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect, Page } from '@playwright/test';
+import fs from 'fs';
+import path = require('path');
 
-// тут вынесена функция на проверку кнопки Да/Нет Альфа релиза в проекте
+// тут вынесена функция на проверку кнопки Да/Нет Альфа релиза в проекте -да
 async function ensureAlphaIsEnabled(page: Page) {
   const alphaLabel = page.getByLabel('Alpha релизы');
   const alphaText = (await alphaLabel.innerText()).trim();
@@ -10,6 +12,18 @@ async function ensureAlphaIsEnabled(page: Page) {
     await expect(page.getByLabel('Alpha релизы')).toContainText('Да');
   }
 }
+
+// тут вынесена функция на проверку кнопки Да/Нет Альфа релиза в проекте -нет
+async function ensureAlphaIsNOEnabled(page: Page) {
+  const alphaLabel = page.getByLabel('Alpha релизы');
+  const alphaText = (await alphaLabel.innerText()).trim();
+
+  if (!alphaText.includes('Нет')) {
+    await page.getByRole('switch', { name: 'Alpha релизы :' }).click();
+    await expect(page.getByLabel('Alpha релизы')).toContainText('Нет');
+  }
+}
+
 // ui токен
 let uiAccessToken: string;
 
@@ -20,8 +34,8 @@ test.describe('Авторизация + Создание Альфы', () => {
   let accessToken: string;
 
   const BASE_URL = 'https://da-dp02-ws-prjm-ift-1.apps.dap.devpub-02.solution.sbt';
-  const USERNAME = 'user';
-  const PASSWORD = 'user';
+  const USERNAME = 'user7';
+  const PASSWORD = 'user7';
 
   test.beforeAll(async ({ request }) => {
     const response = await request.post(`${BASE_URL}/public/v1/auth`, {
@@ -56,9 +70,9 @@ test.describe('Авторизация + Создание Альфы', () => {
   test('Вклюение Альфа признака в проекте', async ({ page }) => {
     await page.goto('https://da-dp02-ws-prjm-ift-1.apps.dap.devpub-02.solution.sbt');
     await page.getByRole('textbox', { name: 'Username or email' }).click();
-    await page.getByRole('textbox', { name: 'Username or email' }).fill('user');
+    await page.getByRole('textbox', { name: 'Username or email' }).fill('user7');
     await page.getByRole('textbox', { name: 'Password' }).click();
-    await page.getByRole('textbox', { name: 'Password' }).fill('user');
+    await page.getByRole('textbox', { name: 'Password' }).fill('user7');
     await page.getByRole('button', { name: 'Sign In' }).click();
 
     //получаем токен
@@ -83,11 +97,14 @@ test.describe('Авторизация + Создание Альфы', () => {
     await page.getByRole('menu').getByText('Продукты').click();
     await page.getByRole('cell', { name: 'Platform V Test Tools' }).click();
 
-    await ensureAlphaIsEnabled(page);
+    await ensureAlphaIsEnabled(page); // добавить шаг на сохранениеawait
+
+    await page.getByRole('button', { name: 'Сохранить save' }).click();
+    await page.locator('div').filter({ hasText: 'Сохранено успешно' }).nth(3).click();
   });
 
   //Создание Альфы- работает
-  test.beforeAll('Создание Альфы с использованием access_token', async ({ request }) => {
+  test('Создание Альфы с использованием access_token', async ({ request }) => {
     const createResponse = await request.post(
       `${BASE_URL}/backend/openapi/v1/productVersions/alfa`,
       {
@@ -117,17 +134,82 @@ test.describe('Авторизация + Создание Альфы', () => {
     expect(createBody.productVersionAlfaId.length).toBeGreaterThan(0);
     expect(createBody.status).toBe(false);
     productVersionAlfaId = createBody.productVersionAlfaId;
+
+    fs.writeFileSync('tmp/productVersionAlfaId.json', JSON.stringify({ productVersionAlfaId }));
+
     console.log(`Создана альфа. productVersionAlfaId = ${productVersionAlfaId}`);
+    const tmpDir = path.resolve('tmp');
+
+    if (!fs.existsSync(tmpDir)) {
+      fs.mkdirSync(tmpDir, { recursive: true });
+    }
+
+    fs.writeFileSync(
+      path.join(tmpDir, 'productVersionAlfaId.json'),
+      JSON.stringify({ productVersionAlfaId }),
+    );
   });
+});
+
+// Проверка созданного альфа релиза -добавить проверку онкретного релиза-вытянуть результат из создания
+
+test('Проверка созданного альфа релиза', async ({ page }) => {
+  const file = fs.readFileSync('tmp/productVersionAlfaId.json', 'utf-8');
+  const { productVersionAlfaId } = JSON.parse(file);
+
+  await page.goto('https://da-dp02-ws-prjm-ift-1.apps.dap.devpub-02.solution.sbt/erp/main');
+  await page.getByRole('textbox', { name: 'Username or email' }).click();
+  await page.getByRole('textbox', { name: 'Username or email' }).fill('user7');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('user7');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByText('SSberbank Technology').click();
+  await page.getByRole('menuitem', { name: 'branches Версии продуктов' }).click();
+  //ищем то что создали
+
+  //
+  await page.locator('.anticon.anticon-setting > svg').click();
+  await page.getByText('ID').click();
+  await page.getByRole('button', { name: 'search' }).first().click();
+  await page.getByRole('textbox', { name: 'Поиск ID' }).click();
+  await page.getByRole('textbox', { name: 'Поиск ID' }).press('CapsLock');
+  await page.getByRole('textbox', { name: 'Поиск ID' }).fill(productVersionAlfaId);
+  await page.getByRole('button', { name: 'Поиск search' }).click();
+  await expect(page.locator('tbody')).toContainText(productVersionAlfaId);
+
+  await page.getByRole('cell', { name: 'TST.61' }).dblclick(); //вставить динамич переменн
+
+  await expect(page.locator('span').filter({ hasText: 'Выпущен' }).first()).toBeVisible();
+  await page.getByText('Platform V Test Tools', { exact: true }).click();
+  await expect(
+    page
+      .locator('div')
+      .filter({ hasText: /^Номер версии$/ })
+      .first(),
+  ).toBeVisible();
+
+  await expect(
+    page
+      .locator('div')
+      .filter({ hasText: /^Platform V Test Tools$/ })
+      .nth(4),
+  ).toBeVisible();
+
+  await expect(page.getByText('Руководитель проектного офиса')).toBeVisible();
+  await page.getByText('Project office').click();
+  await page.getByText('Тестовый Пользователь №3').click();
+  //await page.getByText('Тестовый Пользователь №4').click();-он не создался
 });
 
 //удаление альфы
 test('Удаление Альфа релиза', async ({ page }) => {
+  const file = fs.readFileSync('tmp/productVersionAlfaId.json', 'utf-8');
+  const { productVersionAlfaId } = JSON.parse(file);
   await page.goto('https://da-dp02-ws-prjm-ift-1.apps.dap.devpub-02.solution.sbt/erp/main');
   await page.getByRole('textbox', { name: 'Username or email' }).click();
-  await page.getByRole('textbox', { name: 'Username or email' }).fill('user');
+  await page.getByRole('textbox', { name: 'Username or email' }).fill('user7');
   await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('user');
+  await page.getByRole('textbox', { name: 'Password' }).fill('user7');
   await page.getByRole('button', { name: 'Sign In' }).click();
   await page.getByText('SSberbank Technology').click();
   await page.getByRole('menuitem', { name: 'branches Версии продуктов' }).click();
@@ -147,66 +229,4 @@ test('Удаление Альфа релиза', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Поиск ID' }).press('CapsLock');
   await page.getByRole('textbox', { name: 'Поиск ID' }).fill(productVersionAlfaId);
   await page.getByRole('button', { name: 'Поиск search' }).click();
-});
-
-// Проверка созданного альфа релиза -добавить проверку онкретного релиза-вытянуть результат из создания
-
-test('Проверка созданного альфа релиза', async ({ page }) => {
-  await page.goto('https://da-dp02-ws-prjm-ift-1.apps.dap.devpub-02.solution.sbt/erp/main');
-  await page.getByRole('textbox', { name: 'Username or email' }).click();
-  await page.getByRole('textbox', { name: 'Username or email' }).fill('user');
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('user');
-  await page.getByRole('button', { name: 'Sign In' }).click();
-  await page.getByText('SSberbank Technology').click();
-  await page.getByRole('menuitem', { name: 'branches Версии продуктов' }).click();
-  //ищем то что создали
-
-  await page.locator('.anticon.anticon-setting > svg').click();
-  await page.getByText('ID').click();
-  await page.getByRole('button', { name: 'search' }).first().click();
-  await page.getByRole('textbox', { name: 'Поиск ID' }).click();
-  await page.getByRole('textbox', { name: 'Поиск ID' }).press('CapsLock');
-  await page.getByRole('textbox', { name: 'Поиск ID' }).fill(productVersionAlfaId);
-  await page.getByRole('button', { name: 'Поиск search' }).click();
-  await expect(page.locator('tbody')).toContainText(productVersionAlfaId);
-
-  await page.getByRole('link', { name: '-alpha11' }).click();
-
-  //доп проверка на версию продукта
-
-  await expect(page.locator('#workSpace')).toContainText('Выпущен');
-  await expect(page.getByLabel('Общая информация')).toContainText('Новый'); //тупая проверка
-  await expect(page.getByLabel('Общая информация')).toContainText('Выпущен'); //тупая проверка
-
-  //проверки внутри альфы
-  await page.getByRole('menuitem', { name: 'branches Версии продуктов' }).click();
-
-  await expect(page.getByRole('cell', { name: 'Тест 2.16.2-alpha7' })).toBeVisible();
-  await expect(page.locator('tbody')).toContainText('Тест 2.16.2-alpha7');
-
-  await page.getByRole('cell', { name: 'Тест 2.16.2-alpha7' }).click();
-
-  await expect(page.locator('span').filter({ hasText: 'Выпущен' }).first()).toBeVisible();
-  await page.getByText('Platform V Test Tools', { exact: true }).click();
-  await expect(
-    page
-      .locator('div')
-      .filter({ hasText: /^Номер версии$/ })
-      .first(),
-  ).toBeVisible();
-
-  await expect(
-    page
-      .locator('div')
-      .filter({ hasText: /^Platform V Test Tools$/ })
-      .nth(4),
-  ).toBeVisible();
-  //await page.locator('.root-0-2-179').first().click(); //упал
-  await expect(page.getByText('Руководитель проектного офиса')).toBeVisible();
-  await page.getByText('Project office').click();
-  await page.getByText('Тестовый Пользователь №3').click();
-  await page.getByText('Тестовый Пользователь №4').click();
-  //добавить проверку на наличие комментария
-  //getByRole('textbox',{name: '*Описание upload question-'})- опционально
 });
